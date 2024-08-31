69fde22c3272fd2a87fc66b32288eb2324d832af
admin <ign@gamil.com>
2024-08-31 14:01:56 +0300

    workflow dispatch

--- tests/test_button.py
+++ tests/test_button.py
@@ -14,3 +14,8 @@ def browser():
 def test_button_exist(browser):
     browser.get("https://www.qa-practice.com/elements/button/simple")
     assert browser.find_element('id', 'submit-id-submit').is_displayed()
+
+
+def test_button_exist_2(browser):
+    browser.get("https://www.qa-practice.com/elements/button/like_a_button")
+    assert browser.find_element('link text', 'Click').is_displayed()
